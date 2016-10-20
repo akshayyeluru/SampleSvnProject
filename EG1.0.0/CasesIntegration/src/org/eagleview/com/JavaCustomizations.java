a11e642808b091c5628b16522d188b1664db76be
saikrishna.ch@appsassociates.com <saikrishna.ch@appsassociates.com@344279da-64f8-46bc-a6db-f7044fc5a060>
2016-10-20 18:12:18 +0000

    Intial Commit bu Sai Krishna

---
+++ EG1.0.0/CasesIntegration/src/org/eagleview/com/JavaCustomizations.java
@@ -0,0 +1,23 @@
+package org.eagleview.com;
+
+import com.tibco.security.AXSecurityException;
+import com.tibco.security.ObfuscationEngine;
+
+
+
+public class JavaCustomizations {
+public static String replaceAvatarUrl(String TicketRequest){
+		
+		String a48=TicketRequest.replace("48x48", "a48x48");
+		String a24=a48.replace("24x24", "a24x24");
+		String a16=a24.replace("16x16","a16x16" );
+		String TicketResponse=a16.replace("32x32","a32x32" );
+		
+		return TicketResponse;
+	}
+public static String decryptPassword(String Password) throws AXSecurityException{
+	String DecryptedPassword=new String(ObfuscationEngine.decrypt(Password));
+	
+	return DecryptedPassword;
+}
+}
